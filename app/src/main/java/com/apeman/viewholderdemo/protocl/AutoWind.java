220e52a19ff46b96151ddb64eea49bd6ad961400
Rango <wangqiang@smzdm.com>
2019-08-23 13:41:41 +0800

    init

---
+++ app/src/main/java/com/apeman/viewholderdemo/protocl/AutoWind.java
@@ -0,0 +1,25 @@
+package com.apeman.viewholderdemo.protocl;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @author Rango on 2019-08-23 wangqiang@smzdm.com
+ */
+@Target(FIELD)
+@Retention(RUNTIME)
+public @interface AutoWind {
+    /**
+     * 绑定View id
+     */
+    int viewId();
+
+    /**
+     * 数据字段名称
+     */
+    String fieldName();
+}
+
---
+++ app/src/main/java/com/apeman/viewholderdemo/protocl/CompileHolder.java
@@ -0,0 +1,7 @@
+package com.apeman.viewholderdemo.protocl;
+
+/**
+ * @author Rango on 2019-08-23 wangqiang@smzdm.com
+ */
+public interface CompileHolder {
+}
